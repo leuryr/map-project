5b9fee1242b23a5c497042db646c7aa750214b7a
Leury Rodriguez <leuryr@yahoo.com>
2016-08-24 14:13:35 -0400

    Add typeEnabled and types properties to harcoded locations. Comment edits. Add initYpe function to initialize typeEnabled value. Use Knockout's subscribe to perform action when selectedValue changes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 var Model = [
-    {name:'Yankee Stadium', geometry: {location:{lat: 40.829622, lng: -73.926173}}},
-    {name:'The Bronx Museum of the Arts', geometry: {location:{lat: 40.831011, lng: -73.919719}}},
-    {name:'Concourse Plaza', geometry: {location:{lat: 40.825227, lng: -73.920491}}},
-    {name:'Bronx County Hall of Justice', geometry: {location:{lat: 40.826258, lng: -73.919298}}},
-    {name:'Bronx Supreme Court', geometry: {location:{lat: 40.826173, lng: -73.923834}}},
-    {name:'Heritage Field', geometry: {location:{lat: 40.827023, lng: -73.927761}}},
-    {name:'Joseph Yancey Track and Field', geometry: {location:{lat: 40.828006, lng: -73.929043}}},
-    {name:'Joyce Kilmer Park', geometry: {location:{lat: 40.828522, lng: -73.922673}}}
+    {name:'Yankee Stadium', geometry: {location:{lat: 40.829622, lng: -73.926173}}, typeEnabled: true, types:[]},
+    {name:'The Bronx Museum of the Arts', geometry: {location:{lat: 40.831011, lng: -73.919719}}, typeEnabled: true, types:[]},
+    {name:'Concourse Plaza', geometry: {location:{lat: 40.825227, lng: -73.920491}}, typeEnabled: true, types:[]},
+    {name:'Bronx County Hall of Justice', geometry: {location:{lat: 40.826258, lng: -73.919298}}, typeEnabled: true, types:[]},
+    {name:'Bronx Supreme Court', geometry: {location:{lat: 40.826173, lng: -73.923834}}, typeEnabled: true, types:[]},
+    {name:'Heritage Field', geometry: {location:{lat: 40.827023, lng: -73.927761}}, typeEnabled: true, types:[]},
+    {name:'Joseph Yancey Track and Field', geometry: {location:{lat: 40.828006, lng: -73.929043}}, typeEnabled: true, types:[]},
+    {name:'Joyce Kilmer Park', geometry: {location:{lat: 40.828522, lng: -73.922673}}, typeEnabled: true, types:[]}
 ];
 
 var initApp = function() {
@@ -22,11 +22,21 @@ var mapFail = function() {
 };
 var ViewModel = function() {
     var self = this;
-    // Create an observable array to store locations.
+    // Create an observable array, and store hardcoded Model locations.
     self.locations = ko.observableArray(Model);
     // Create an onservable for the selected filter value.
     self.selectedValue = ko.observable();
-    // This creates map markers from an array of locations.
+    self.selectedValue.subscribe(function(newValue) {
+        console.log(newValue);
+        self.locations().forEach(function(item) {
+            if(item.types.includes(newValue)) {
+                item.typeEnabled = true;
+            } else {
+                item.typeEnabled = false;
+            };
+        });
+    });
+    // This function creates map markers from an observable array of locations.
     self.createMarkers = function(array) {
         array().forEach(function(item) {
             if (!(item.hasOwnProperty('marker'))) {
@@ -46,6 +56,11 @@ var ViewModel = function() {
             }
         });
     };
+    // This function sets the initial value of the typeEnabled property
+    // for a location to true.
+    self.initType = function(item) {
+        item.typeEnabled = true;
+    };
     // This calls the Maps API Places service.
     var service = new google.maps.places.PlacesService(map);
     // Creating an object with fields required for our future nearbySearch method.
@@ -54,10 +69,13 @@ var ViewModel = function() {
         radius: 500,
         types: ['bakery','bank','bar','cafe','gym','movie_theater','restaurant',]
     };
+    // This callback function will be used to handle the results of the
+    // Places nearbySearch method.
     var requestCallback = function(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             console.log(results);
             results.forEach(function(result) {
+                self.initType(result);
                 // Pushing results to our observable array.
                 self.locations.push(result);
             });
@@ -65,6 +83,8 @@ var ViewModel = function() {
             self.createMarkers(self.locations);
         } else {
             console.log('Could not complete search!');
+            // Creates markers for hardcoded locations if Places service doesn't work.
+            self.createMarkers(self.locations);
             };
     };
     // Call to nearbySearch method for Places.
@@ -75,7 +95,7 @@ var ViewModel = function() {
         var marker = object.marker;
         self.animateMarker(marker);
     };
-    // This function passes in the a marker
+    // This function passes in a marker
     // and gives it a boucing animantion.
     self.animateMarker = function(marker) {
         marker.setAnimation(google.maps.Animation.BOUNCE);
