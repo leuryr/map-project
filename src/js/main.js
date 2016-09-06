c40c42a409f4fb213ad9c677a9a836501bf7d231
Leury Rodriguez <leuryr@yahoo.com>
2016-09-06 12:50:07 -0400

    Add Info Window feature.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 var Model = [
-    {name:'Yankee Stadium', geometry: {location:{lat: 40.829622, lng: -73.926173}}, typeEnabled: ko.observable(true), types:["stadium"]},
-    {name:'The Bronx Museum of the Arts', geometry: {location:{lat: 40.831011, lng: -73.919719}}, typeEnabled: ko.observable(true), types:[]},
-    {name:'Concourse Plaza', geometry: {location:{lat: 40.825227, lng: -73.920491}}, typeEnabled: ko.observable(true), types:["shopping_mall"]},
-    {name:'Bronx County Hall of Justice', geometry: {location:{lat: 40.826258, lng: -73.919298}}, typeEnabled: ko.observable(true), types:["courthouse"]},
-    {name:'Bronx Supreme Court', geometry: {location:{lat: 40.826173, lng: -73.923834}}, typeEnabled: ko.observable(true), types:["courthouse"]},
-    {name:'Heritage Field', geometry: {location:{lat: 40.827023, lng: -73.927761}}, typeEnabled: ko.observable(true), types:["park"]},
-    {name:'Joseph Yancey Track and Field', geometry: {location:{lat: 40.828006, lng: -73.929043}}, typeEnabled: ko.observable(true), types:["park"]},
-    {name:'Joyce Kilmer Park', geometry: {location:{lat: 40.828522, lng: -73.922673}}, typeEnabled: ko.observable(true), types:["park"]}
+    {name:"Yankee Stadium", geometry: {location:{lat: 40.829622, lng: -73.926173}}, typeEnabled: ko.observable(true), types:["stadium"], vicinity: "1 E 161st St, Bronx"},
+    {name:"The Bronx Museum of the Arts", geometry: {location:{lat: 40.831011, lng: -73.919719}}, typeEnabled: ko.observable(true), types:["museum"], vicinity: "1040 Grand Concourse, Bronx"},
+    {name:"Concourse Plaza", geometry: {location:{lat: 40.825227, lng: -73.920491}}, typeEnabled: ko.observable(true), types:["shopping_mall"], vicinity: "220 E 161st St, Bronx"},
+    {name:"Bronx County Hall of Justice", geometry: {location:{lat: 40.826258, lng: -73.919298}}, typeEnabled: ko.observable(true), types:["courthouse"], vicinity: "265 E 161st St, Bronx"},
+    {name:"Bronx Supreme Court", geometry: {location:{lat: 40.826173, lng: -73.923834}}, typeEnabled: ko.observable(true), types:["courthouse"], vicinity: "851 Grand Concourse #111, Bronx"},
+    {name:"Heritage Field", geometry: {location:{lat: 40.827023, lng: -73.927761}}, typeEnabled: ko.observable(true), types:["park"], vicinity: ", Bronx"},
+    {name:"Joseph Yancey Track and Field", geometry: {location:{lat: 40.828006, lng: -73.929043}}, typeEnabled: ko.observable(true), types:["park"], vicinity: ", Bronx"},
+    {name:"Joyce Kilmer Park", geometry: {location:{lat: 40.828522, lng: -73.922673}}, typeEnabled: ko.observable(true), types:["park"], vicinity: "Walton Ave, Bronx"}
 ];
 
 var initApp = function() {
@@ -29,17 +29,25 @@ var ViewModel = function() {
     self.selectedValue.subscribe(function(newValue) {
         console.log(newValue);
         self.locations().forEach(function(item) {
-            if(item.types.includes(newValue)) {
+            if(item.types.includes(newValue) || (newValue == "Choose a type")) {
                 item.typeEnabled(true);
                 item.marker.setVisible(true);
             } else {
-                if(newValue !== "Choose a type") {
                     item.typeEnabled(false);
                     item.marker.setVisible(false);
                 };
-            };
         });
     });
+    var infoWindowService = new google.maps.InfoWindow();
+    self.fillInfoWindow = function(location, infoWindow) {
+        if(infoWindow.marker != location.marker) {
+            infoWindow.marker = location.marker;
+            var address = location.vicinity;
+            var firstLine = address.substring(0,address.indexOf(","));
+            infoWindow.setContent('<div>' + location.name + '<hr>' + '<br>' + firstLine + '<br>Bronx, NY</div>');
+            infoWindow.open(map, location.marker);
+        };
+    };
     // This function creates map markers from an observable array of locations.
     self.createMarkers = function(array) {
         array().forEach(function(item) {
@@ -55,6 +63,7 @@ var ViewModel = function() {
                 });
                 marker.addListener('click', function() {
                     self.animateMarker(this);
+                    self.fillInfoWindow(item, infoWindowService);
                 });
                 item.marker = marker;
             }
@@ -69,12 +78,12 @@ var ViewModel = function() {
         item.typeEnabled = ko.observable(true);
     };
     // This calls the Maps API Places service.
-    var service = new google.maps.places.PlacesService(map);
+    var places = new google.maps.places.PlacesService(map);
     // Creating an object with fields required for our future nearbySearch method.
     var searchRequest = {
         location: Model[7].geometry.location,
         radius: 500,
-        types: ['bakery','bank','bar','cafe','gym','movie_theater','restaurant',]
+        types: ['bank','bar', 'gym','movie_theater','restaurant',]
     };
     // This callback function will be used to handle the results of the
     // Places nearbySearch method.
@@ -94,12 +103,15 @@ var ViewModel = function() {
             };
     };
     // Call to nearbySearch method for Places.
-    service.nearbySearch(searchRequest, requestCallback);
+    places.nearbySearch(searchRequest, requestCallback);
     // This function gets the marker from the location object
     // and passes it into the animateMarker function.
     self.getMarker = function(object) {
         var marker = object.marker;
         self.animateMarker(marker);
+    };
+    self.getInfoWindow = function(object) {
+        self.fillInfoWindow(object, infoWindowService);
     };
     // This function passes in a marker
     // and gives it a boucing animantion.
